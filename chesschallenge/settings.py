5c545737ce4f17acd2261f5958c7c33b7f650834
mikaio damaceno <mikaio.damacemo@tradexsolutions.com.br>
2021-09-01 19:56:21 -0300

    fix:correcting_settings

--- chesschallenge/settings.py
+++ chesschallenge/settings.py
@@ -62,8 +62,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chesschallenge.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
