1503883e3a925d63113fad73599972078fd62932
mikaio damaceno <mikaio.damacemo@tradexsolutions.com.br>
2021-09-02 12:13:51 -0300

    fix:removing_sqlite_settings

--- chesschallenge/settings.py
+++ chesschallenge/settings.py
@@ -65,22 +65,14 @@ WSGI_APPLICATION = 'chesschallenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "db",
-#         "PORT": 5432,
-#     }
-# }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
